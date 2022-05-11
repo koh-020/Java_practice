7424af31bd29772a87c8aaba78632a4c1d727506
koh-020 <97607232+koh-020@users.noreply.github.com>
2022-05-11 17:33:02 +0900

    add if

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "Koh";
+		System.out.println(name);
+		final int LUCKY_NUM = 3;
+		System.out.println(LUCKY_NUM);
+	}
+
+}
---
+++ Chapter05.java
@@ -0,0 +1,25 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long num1 = 40L;
+		double num2 = 3.14;
+		boolean ok = true;
+		System.out.println(num1);
+		System.out.println(num2);
+		System.out.println(ok);
+		Float num3 = 9.8066f;
+		Integer num4 = 5;
+		Character char1 = 'A';
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(char1);
+		int[] array1 = new int[5];
+		array1[0] = 3;
+		array1[1] = 4;
+		array1[2] = 5;
+		array1[3] = 6;
+		array1[4] = 7;
+		System.out.println(array1[3]);
+		
+	}
+}
